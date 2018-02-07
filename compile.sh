764c1f7d2e85589e4aa74750fb7b3e00c98b3eff
Brad Caron <bacaron@imail.iu.edu>
2018-02-07 15:37:06 -0500

    recompiled

--- compile.sh
+++ compile.sh
@@ -6,6 +6,7 @@ mkdir -p compiled
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
 mcc -m -R -nodisplay -d compiled main
 exit
 END
